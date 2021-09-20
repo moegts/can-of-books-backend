cf4a73d7ad4dde28eca2a107bc7ecb89ab14b62f
Mohammad Khalil <moegts@gmail.com>
2021-09-21 02:57:34 +0300

    fix more things and add delete and add functions on mongodb

--- controllers/Seeder.controller.js
+++ controllers/Seeder.controller.js
@@ -1,15 +1,32 @@
 'use strict';
 const {SeederModel} = require("../models/Seeder.model")
+require("../models/Book.model")
 
-let seederController = (req,res)=>{
-    console.log('🟢',req);
-    let bookid = req.query.id
-    SeederModel.findOne({_id:bookid}).then(data=>{
+let seederController = (req, res) => {
+    SeederModel.find().then(data => {
         res.json(data);
     })
-    // SeederModel.find().then(data=>{
-    //     res.json(data);
-    // })
 }
 
-module.exports= {seederController}
+const createBookController = async (req, res) =>{
+    let {title, description, email} = req.body;
+    let newCreate = new SeederModel({
+        title:title,
+        description:description,
+        email:email,
+    })
+    newCreate.save();
+    let bookList = await SeederModel.find({});
+    res.status(201).json(bookList);
+}
+
+const deleteBookController = (req, res) =>{
+    let id = req.params.id;
+    SeederModel.findByIdAndDelete(id, async (err, data) => {
+        if (err) res.status(500).send("error 505");
+        let bookList = await SeederModel.find({})
+        res.json(bookList);
+    })
+}
+
+module.exports= {seederController, createBookController, deleteBookController}
